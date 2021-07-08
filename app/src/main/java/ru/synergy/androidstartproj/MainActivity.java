5748c80b61de15edd388cb79859191f079bb7179
artemmad <artemmakarevich1997@gmail.com>
2021-07-08 13:13:14 +0300

    We have learned LinearLayout

--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -11,12 +11,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
+        setContentView(R.layout.activity_main);
 
 
-        TextView textView = (TextView) findViewById(R.id.header);
-
-        textView.setText("Hello from Java");
+//        TextView textView = (TextView) findViewById(R.id.header);
+//
+//        textView.setText("Hello from Java");
 
 //
 //        ConstraintLayout constraintlayout = new ConstraintLayout(this);
