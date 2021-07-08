26b4a37e2c4f22619ac85d6a7a231cb9dc6c928c
artemmad <artemmakarevich1997@gmail.com>
2021-07-08 19:23:18 +0300

    Our first calculator done

--- app/src/main/java/ru/synergy/androidstartproj/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproj/Calculator.java
@@ -3,6 +3,7 @@ package ru.synergy.androidstartproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Calculator extends AppCompatActivity {
+
+    private static final String LogcatTag = "CALCULATOR_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class Calculator extends AppCompatActivity {
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(LogcatTag, "Button have been pushed");
                 calcuclateAnswe();
             }
         });
@@ -38,27 +42,41 @@ public class Calculator extends AppCompatActivity {
 
         TextView answer = (TextView) findViewById(R.id.result);
 
+        Log.d(LogcatTag, "All views have been founded");
+
         float numone = Integer.parseInt(numOne.getText().toString());
         float numtwo = Integer.parseInt(numTwo.getText().toString());
+
+        Log.d(LogcatTag, "Successfully grabbed data from input fields");
+        Log.d(LogcatTag, "numone is: " + numone + " ; "+" numtwo is: " + numtwo);
 
         float solution = 0;
 
         if(add.isChecked()){
+            Log.d(LogcatTag, "Operation is add");
             solution = numone + numtwo;
         }
         if(sub.isChecked()){
+            Log.d(LogcatTag, "Operation is sub");
             solution = numone - numtwo;
         }
         if(multiply.isChecked()){
+            Log.d(LogcatTag, "Operation is multiply");
             solution = numone * numtwo;
         }
         if(divide.isChecked()){
+            Log.d(LogcatTag, "Operation is divide");
             if(numtwo == 0 ){
                 Toast.makeText(this, "Number two Cannot be zero", Toast.LENGTH_SHORT).show();
                 return;
             }
             solution = numone / numtwo;
         }
+
+        Log.d(LogcatTag, "The result of operations is: " + solution);
+
+
+       // Log.wtf() // What a Terrible Failure == error
 
         answer.setText("The answer is "+ solution);
 
